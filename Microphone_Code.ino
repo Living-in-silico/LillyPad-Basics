93cc5ed7c93e8d10aa6130596060d3d0bab56de9
D-Harsh <harsh200014@gmail.com>
2018-08-10 15:03:43 -0400

    Add files via upload

---
+++ Microphone_Code.ino
@@ -0,0 +1,21 @@
+const int sensorPin = A1; 
+int sensorValue = 0;
+int TU = 60;
+int TD = 57;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(sensorPin, INPUT); 
+}
+
+void loop() {
+  sensorValue = analogRead(sensorPin);
+//  Serial.println(sensorValue);
+  if (sensorValue > TU || sensorValue < TD){
+  Serial.println(sensorValue-59);
+  }
+  else{
+    Serial.println(0);
+  }
+  delay(2);
+}
